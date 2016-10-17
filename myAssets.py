8550c924de59239cd70aa7e9dc8d313e9fc07486
Mike Rosseel <mike@eon-consult.be>
2016-10-17 21:08:35 +0200

    add sample asset file

---
+++ myAssets.py
@@ -0,0 +1,12 @@
+# helper values - add yours to help calculate gold_ounces / silver_ounces
+krugerrand_troy_ounce = 1.0909
+maple_leaf_tube_ounce = 25
+
+# main values needed as input
+stocks = ['cspx.as','iwda.as']
+stocks_amounts = [5,10]
+crypto_poloniex= ['XMR', 'ETH']
+crypto_poloniex_amounts=[100,99]
+bitcoin = 2.3
+gold_ounces=krugerrand_troy_ounce*2
+silver_ounces=maple_leaf_tube_ounce*2
