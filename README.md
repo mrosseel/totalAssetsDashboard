ece42ca8cfa9a6911f9367bdb1ebd01bb653a7fa
Mike Rosseel <mike@eon-consult.be>
2017-04-27 09:16:14 +0200

    catch gold/silver errors

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Gather current prices for a variety of assets and display them as one aggregate dollar/euro value
 
 TODO:
+- add main method + accept parameters to point to other asset file + change eur/usd?
 - generate html file which shows the number
--- assets.py
+++ assets.py
@@ -1,17 +1,21 @@
-import myAssets as myAssets
+import assetsMike as myAssets
 import requests
 from requests import ConnectionError
 import numpy as np
+import argparse
+import time
 
-def getStocks():
+def getStocks(verbose=False):
     r = requests.get('http://download.finance.yahoo.com/d/quotes.csv?s={0}&f=l1'.format(','.join(myAssets.stocks)))
     text = r.text.split('\n')
+    print text
     result = map(lambda x: float(str(x)), text[:-1])
     holdings = np.multiply(result,myAssets.stocks_amounts)
     total = sum(holdings)
+    if verbose: print("stocks",total)
     return total
 
-def getCrypto():
+def getCrypto(verbose=False):
     total = 0
     krakeneur = requests.get('https://api.kraken.com/0/public/Ticker?pair=XBTEUR')
     btceur = float(str(krakeneur.json()['result']['XXBTZEUR']['c'][0]))
@@ -20,27 +24,48 @@ def getCrypto():
     for currency, amount in zip(myAssets.crypto_poloniex, myAssets.crypto_poloniex_amounts):
         lastcur = float(str(crypto.json()['BTC_{0}'.format(currency)]['last']))
         total += lastcur*amount*btceur
+    if verbose: print("crypto",total)
     return total
 
-def getPM():
+def getPM(verbose=False):
     # gold
     gold = requests.get('https://www.quandl.com/api/v1/datasets/LBMA/GOLD.json')
-    golddate = str(gold.json()['data'][0][0])
     try:
         goldpriceeur = float(str(gold.json()['data'][0][-1]))
     except ValueError as e:
         print "unable to fetch gold price"
-        goldpriceeur = 0
-
+        goldpriceeur = float(str(gold.json()['data'][0][-2]))
+    except KeyError as e:
+	print(gold.json())
+	print "no gold data"
+	goldpriceeur = 1000
 
     # silver
-    silver = requests.get('https://www.quandl.com/api/v1/datasets/LBMA/SILVER.json')
-    silverdate = str(silver.json()['data'][0][0])
-    silverpriceeur = float(str(silver.json()['data'][0][-1]))
+    try:
+        silver = requests.get('https://www.quandl.com/api/v1/datasets/LBMA/SILVER.json')
+        silverdate = str(silver.json()['data'][0][0])
+        silverpriceeur = float(str(silver.json()['data'][0][-1]))
+    except KeyError as e:
+        print "no silver data"
+        silverpriceeur = 16
 
-    return myAssets.gold_ounces*goldpriceeur + myAssets.silver_ounces*silverpriceeur
+    total = myAssets.gold_ounces*goldpriceeur + myAssets.silver_ounces*silverpriceeur
+    if verbose: print("pm",total)
+    return total
+
+def getOther(verbose=False):
+    # cash euro
+    total =  myAssets.cash_euro
+    if verbose: print("cash_euro",total)
+    return total
 
 try:
-    print(getStocks()+getCrypto()+getPM()+cash)
+    parser = argparse.ArgumentParser(description='Asset arguments.')
+    parser.add_argument('--silent', dest='silent', action='store_true')
+    parser.set_defaults(silent=False)
+    args = parser.parse_args()
+    verbose = not args.silent
+    if verbose: print(time.strftime("%x %H:%M:%S"))
+    print(getStocks(verbose)+getCrypto(verbose)+getPM(verbose)+getOther(verbose))
 except ConnectionError as e:
     print "could not connect to internet"
