9c677a04cc2b4dfcc574ce5931cfeef413b60e62
Mike Rosseel <mike.rosseel@gmail.com>
2017-09-24 21:24:20 +0200

    Added python server and refactoring
    Refactoring to split getting the prices from the calculation using
    actual amounts. Added a server to serve the PM prices, because those
    data feeds were too big for embedded and no alternatives were found in
    a reasonable time.

--- assets.py
+++ assets.py
@@ -1,45 +1,58 @@
 import assetsMike as myAssets
 import requests
 from requests import ConnectionError
-import numpy as np
 import argparse
 import time
 import locale
+import json
 
-def getStocks(verbose=False):
-    r = requests.get('http://download.finance.yahoo.com/d/quotes.csv?s={0}&f=l1'.format(','.join(myAssets.stocks)))
+# arg is array of stocks
+def getStocksRaw(myStocks):
+    r = requests.get('http://download.finance.yahoo.com/d/quotes.csv?s={0}&f=l1'.format(myStocks))
     text = r.text.split('\n')
-    #print text
-    result = map(lambda x: float(str(x)), text[:-1])
-    holdings = np.multiply(result,myAssets.stocks_amounts)
+    result = list(map(lambda x: float(str(x)), text[:-1]))
+    return result
+
+def getStocks(myStocks):
+    total = 0
+    result = getStocksRaw(','.join(myStocks))
+    holdings = [a*b for a,b in zip(result, myAssets.stocks_amounts)]
     total = sum(holdings)
-    printResult("stocks", total)
     return total
 
-def getCrypto(verbose=False):
-    total = 0
+def getCryptoRaw(myPoloniexCrypto):
     krakeneur = requests.get('https://api.kraken.com/0/public/Ticker?pair=XBTEUR')
     btceur = float(str(krakeneur.json()['result']['XXBTZEUR']['c'][0]))
-    total += myAssets.bitcoin * btceur
     crypto = requests.get('https://poloniex.com/public?command=returnTicker')
+    result = {'btceur': btceur}
+    for currency in myAssets.crypto_poloniex:
+        result[currency] = float(str(crypto.json()['BTC_{0}'.format(currency)]['last']))
+    return result
+
+def getCrypto(myPoloniexCrypto):
+    total = 0
+    dictresult = getCryptoRaw(myPoloniexCrypto)
+    btceur = dictresult['btceur']
+    del dictresult['btceur']
+    total += myAssets.bitcoin * btceur
     for currency, amount in zip(myAssets.crypto_poloniex, myAssets.crypto_poloniex_amounts):
-        lastcur = float(str(crypto.json()['BTC_{0}'.format(currency)]['last']))
-        total += lastcur*amount*btceur
-    printResult("crypto", total)
+        total += dictresult[currency]*amount*btceur
     return total
 
-def getPM(verbose=False):
+def getPMRaw():
     # gold
     gold = requests.get('https://www.quandl.com/api/v1/datasets/LBMA/GOLD.json')
     try:
         goldpriceeur = float(str(gold.json()['data'][0][-1]))
     except ValueError as e:
-        print "unable to fetch gold price"
-        goldpriceeur = float(str(gold.json()['data'][0][-2]))
+        try:
+            goldpriceeur = float(str(gold.json()['data'][0][-2]))
+        except:
+            goldpriceeur = 1000
     except KeyError as e:
-	print(gold.json())
-	print "no gold data"
-	goldpriceeur = 1000
+        print(gold.json())
+        print("no gold data")
+        goldpriceeur = 1000
 
     # silver
     try:
@@ -47,30 +60,59 @@ def getPM(verbose=False):
         silverdate = str(silver.json()['data'][0][0])
         silverpriceeur = float(str(silver.json()['data'][0][-1]))
     except KeyError as e:
-        print "no silver data"
+        print("no silver data")
         silverpriceeur = 16
+    return {'goldeur': goldpriceeur, 'silvereur': silverpriceeur}
 
-    total = myAssets.gold_ounces*goldpriceeur + myAssets.silver_ounces*silverpriceeur
-    printResult("pm", total)
+def getPMRawJson():
+    return json.dumps(getPMRaw())
+
+def getPM(verbose=False):
+    dictresult = getPMRaw()
+    total = myAssets.gold_ounces*dictresult['goldeur'] + myAssets.silver_ounces*dictresult['silvereur']
     return total
 
 def getOther(verbose=False):
     # cash euro
     total =  myAssets.cash_euro
-    printResult("Cash", total)
+    #printResult("Cash", total)
     return total
 
-def printResult(name, value):
-    if verbose: print('{}:\t\t{:>7} EUR'.format(name, locale.format('%.0f', value, True, True)))
+def printFullResult(name, value, total):
+    printResult(True, name, value)
+    printPercentage(True, value, total)
 
-try:
-    locale.setlocale(locale.LC_ALL, 'de_DE')
-    parser = argparse.ArgumentParser(description='Asset arguments.')
-    parser.add_argument('--silent', dest='silent', action='store_true')
-    parser.set_defaults(silent=False)
-    args = parser.parse_args()
-    verbose = not args.silent
+def printResult(name, value):
+    print('{}:\t\t{:>7} EUR'.format(name, locale.format('%.0f', value, True, True)), end='')
+
+def printPercentage(value, total):
+    print('\t\t({:>4} %)'.format(locale.format('%.1f', value/total*100, True, True)))
+
+def printAll(verbose=True):
     if verbose: print('Datetime:\t{}\n'.format(time.strftime("%x @ %H:%M:%S")))
-    printResult('Total', getStocks(verbose)+getCrypto(verbose)+getPM(verbose)+getOther(verbose))
-except ConnectionError as e:
-    print "could not connect to internet"
+    stocks = getStocks(myAssets.stocks)
+    crypto = getCrypto(myAssets.crypto_poloniex)
+    other = getOther()
+    pm = getPM()
+    total = stocks+crypto+pm+other
+    printFullResult("stocks", stocks, total)
+    printFullResult("crypto", crypto, total)
+    printFullResult("pm", pm, total)
+    printFullResult("other", other, total)
+    printResult('Total', total)
+    print()
+
+def run():
+    try:
+        locale.setlocale(locale.LC_ALL, 'de_DE')
+        parser = argparse.ArgumentParser(description='Asset arguments.')
+        parser.add_argument('--silent', dest='silent', action='store_true')
+        parser.set_defaults(silent=False)
+        args = parser.parse_args()
+        verbose = not args.silent
+        printAll(verbose)
+    except ConnectionError as e:
+        print("could not connect to internet")
+
+if __name__ == '__main__':
+    run()
--- totalAssets.sh
+++ totalAssets.sh
@@ -1,1 +1,1 @@
-python assets.py >> progress.txt && tail -n 7 progress.txt
+python3 assets.py >> progress.txt && tail -n 7 progress.txt
